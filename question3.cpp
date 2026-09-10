61c071d0d34419514372dff190c88cb319dc69ed
work-aditya <workforomeg4@gmail.com>
2026-09-10 13:25:04 +0530

    added new question

---
+++ question3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class solution {
+
+    public :
+    
+};
+
+int main(){
+    int a =4 , b =6;
+    int sum = a+b;
+    return sum;
+}
