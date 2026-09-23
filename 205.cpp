e680e6ef972e8537956ce99376061291c5ce5efe
work-aditya <workforomeg4@gmail.com>
2026-09-23 11:53:10 +0530

    added 205 question leetcode

---
+++ 205.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+class solution {
+    public :
+    bool isIsomorphic(string s, string t){
+        unordered_map <char , char> mps;
+        unordered_map <char , char > mpt;
+
+
+        for (int i =0; i < s.length(); i++){
+            char a = s[i];
+            char b = t[i];
+            
+        }
+
+
+    }
+
+};
+
+int main(){
+
+}
+
